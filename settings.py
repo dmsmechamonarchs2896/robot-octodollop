684f1e5bd3333209427c0c4189df30e3a32a25af
Marquis Kurt <marquiskurt@outlook.com>
2018-01-12 07:08:56 -1000

    Replace usernames with real names in README

---
+++ settings.py
@@ -0,0 +1,10 @@
+# settings
+# Created by marquiskurt
+# robot-octodollop (C) 2018 MechaMonarchs Team.
+# Licensed under Apache 2.0 License
+#
+# Offers a dialog for changing settings such as position and alliance
+import PySide
+
+if __name__ == '__main__':
+    print("Hi, Monika.")
